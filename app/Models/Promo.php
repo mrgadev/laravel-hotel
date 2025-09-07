c213edb8e1a8b19f935ebaa245662ee6d6c0020d
Muhammad Rizqi <mrizqighana@outlook.co.id>
2025-09-08 04:16:34 +0700

    fix promo

--- app/Models/Promo.php
+++ app/Models/Promo.php
@@ -8,11 +8,11 @@ class Promo extends Model
 {
     protected $fillable = ['name', 'code', 'cover', 'amount','start_date', 'end_date', 'is_all'];
 
-    // public function rooms(){
-    // return $this->belongsToMany(Room::class, 'promo_room', 'promo_id', 'room_id');
-    // }
+    public function rooms(){
+    return $this->belongsToMany(Room::class, 'promo_room', 'promo_id', 'room_id');
+    }
 
-    // public function transactions() {
-    //     return $this->belongsToMany(Transaction::class, 'transaction_promos');
-    // }
+    public function transactions() {
+        return $this->belongsToMany(Transaction::class, 'transaction_promos');
+    }
 }
